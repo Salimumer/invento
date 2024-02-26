ad4f24ae8d1e5f9238a923194f085e67ba2e548c
SALIM UMER <salimbinumer@gmail.com>
2024-02-26 07:26:49 +0530

    intiate

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import path from "path"
+import react from "@vitejs/plugin-react"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // ...,
+    TanStackRouterVite(),
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
